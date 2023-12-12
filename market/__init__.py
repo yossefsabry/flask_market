b7dcd32854514c9851345e02eea9e6d1c758c65d
yossefsabry <yossefsabry66@gmail.com>
2023-12-12 20:21:19 +0200

    valid form

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 # start config for the db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'd65337e2c9b74480cc144ebf93b9d16711b106b9'
+
+app.config['CSRF_ENABLED'] = True
+app.debug=True
 # make instance from sqlalchemy class 
 db = SQLAlchemy(app)
 
--- market/routes.py
+++ market/routes.py
@@ -1,11 +1,12 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from market.models import Item
 from market.forms import RegisterFrom
 from market import db
 from market.models import User
 
 @app.route('/')
+@app.route('/home')
 def home_page():
     return render_template("home.html")
 
@@ -15,19 +16,24 @@ def market_page():
     return render_template("market.html" , items=items)
 
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterFrom()
     
-    if form.is_submitted(): # check on sumbit
-        
-        create_user = User(username=form.username.data, email_address = form.email_address.data, password_hash = form.password1.data)
-        
-        db.session.add(create_user)
-        db.session.commit()
-        
+    if request.method == 'POST':
+        if form.validate_on_submit(): # check on sumbit
+            print("helllo______________________________________")
+        try:
+            create_user = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+            db.session.add(create_user)
+            db.session.commit()
+            
+        except Exception as e:
+            print(f"Error creating user: {e}")
+            return render_template("register.html", form=form)
+
+        print("market page-----------------------")
         return redirect(url_for('market_page'))
-    
+        
     return render_template("register.html",  form=form)
-
-
